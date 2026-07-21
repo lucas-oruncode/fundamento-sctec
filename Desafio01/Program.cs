ca1214a64009aecfa652fce4344d9715f88840a1
Lucas de Jesus <oruncode@gmail.com>
2026-07-20 21:00:08 -0300

    feat: desafio1 and desafio2

--- Desafio01/Program.cs
+++ Desafio01/Program.cs
@@ -3,3 +3,29 @@
 // O usuário tem 3 tentativas
 // Se errar 3 vezes, o sistema bloqueia o acesso
 // Se acertar, exibe "Login realizado com sucesso"
+
+string user = "usuario";
+string password = "senha123";
+int attempts = 3;
+
+do
+{
+
+    Console.Write("Informe o usuário: ");
+    user = Console.ReadLine();
+
+    Console.Write("Informe a senha: ");
+    password = Console.ReadLine();
+
+    if (user == "usuario" && password == "senha123")
+    {
+        Console.WriteLine("Login realizado com sucesso!");
+        attempts = 0;
+    } else
+    {
+        Console.WriteLine($"Usuário ou senha incorreto. Tente novamente. \nTentativas: {attempts}");
+        attempts--;
+        string message = (attempts < 0) ? "Acesso bloqueado!" : "";
+        Console.WriteLine(message);
+    }
+} while (attempts >= 0);
--- Desafio02/Program.cs
+++ Desafio02/Program.cs
@@ -8,3 +8,88 @@
 // Menu deve aparecer pelo menos uma vez
 // Saldo não pode ficar negativo
 // O usuário deve conseguir fazer várias operações até escolher sair
+
+int menuOption;
+decimal totalValue = 0;
+
+do
+{
+    Console.WriteLine("==================================");
+    Console.WriteLine("Bem vindo ao Caixa Eletrônico \nInforme a opção desejada:");
+    Console.WriteLine("1. Depositar \n2. Sacar \n3. Ver saldo \n4. Sair");
+    bool isValid = int.TryParse(Console.ReadLine(), out menuOption);
+
+    if (!isValid)
+    {
+        Console.WriteLine("Opção inválida");
+    }
+
+    switch (menuOption)
+    {
+        case 1:
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("1. DEPOSITO \nInforme o valor que deseja depositar:");
+            isValid = decimal.TryParse(Console.ReadLine(), out decimal deposit);
+
+            if (isValid)
+            {
+                totalValue += deposit;
+                Console.WriteLine($"Você depositou {deposit:C2}");
+            } 
+            else
+            {
+                Console.WriteLine("Valor inválido.");
+            }
+
+            break;
+
+        case 2:
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("2. SACAR \nInforme o valor que deseja sacar");
+            isValid = decimal.TryParse(Console.ReadLine(), out decimal withdraw);
+
+            if (isValid)
+            {
+                if (totalValue >= withdraw)
+                {
+                    totalValue -= withdraw;
+                    Console.WriteLine($"Você sacou {withdraw:C2}");
+                }
+                else
+                {
+                    Console.WriteLine("Saldo insuficiente.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido.");
+            }
+
+            break;
+
+        case 3:
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine($"3. SALDO DISPONIVEL \nSaldo: {totalValue:C2}");
+
+            break;
+        
+        case 4:
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Encerrando operações...");
+
+            break;
+
+        default:
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Opção inválida");
+
+            break;
+    }
+
+
+} while(menuOption != 4);
