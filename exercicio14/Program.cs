1be1716a2c9507f9188e1dafc79329a9c86b0903
Lucas de Jesus <oruncode@gmail.com>
2026-07-21 20:38:31 -0300

    feat: new exercises

---
+++ exercicio14/Program.cs
@@ -0,0 +1,19 @@
+﻿// Faça um programa que pergunte o tamanho de um array de string
+// Crie o array e depois  preencha-o utilizando uma estrutura de repetição
+// Após preenchido exiba o array na tela para o usuário
+
+Console.WriteLine("Informe o tamanho de array que você deseja:");
+int arraySize = int.Parse(Console.ReadLine());
+
+var array = new string[arraySize];
+
+for (int i = 0; i < arraySize; i++)
+{
+    Console.WriteLine($"Informe a palavra {i+1} que deseja armazenar");
+    array[i] = Console.ReadLine();
+}
+
+for (int i = 0; i < arraySize; i++)
+{
+    Console.WriteLine($"Palavra {i+1}: {array[i]}");
+}
---
+++ exercicio15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Crie um programa que pergunte quantas notas serão adicionadas
+// Depois o usuário digita as notas
+// Por fim retorne a média das notas
+
+
+Console.WriteLine("Informe quantas notas serão adicionadas:");
+int numGrades = int.Parse(Console.ReadLine());
+
+var grades = new double[numGrades];
+double sum = 0;
+
+for (int i = 0; i < numGrades; i++)
+{
+    Console.WriteLine($"Informe a nota {i+1}");
+    grades[i] = double.Parse(Console.ReadLine());
+
+    sum += grades[i];
+}
+
+for (int i = 0; i < numGrades; i++)
+{
+    Console.WriteLine($"Nota {i+1}: {grades[i]} \n-------------");
+}
+
+Console.WriteLine($"Média: {sum / numGrades:F3}");
